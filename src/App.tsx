67e8ba89b290e3c0b5640d3c72656f08d73d59bb
Viacheslav <codeexplorer7@gmail.com>
2022-10-05 18:13:59 +0300

    Whole app logic added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,123 @@
 import React from "react";
 import './App.css';
+import { useState } from 'react';
+
+
+function App() {
+  const questions = [
+    {
+      questionText: 'Столица США?',
+      answerOptions: [
+        { answerText: 'Бостон', isCorrect: false },
+        { answerText: 'Мадрид', isCorrect: false },
+        { answerText: 'Краснодар', isCorrect: false },
+        { answerText: 'Вашингтон', isCorrect: true }
+      ]
+    },
+    {
+      questionText: 'В каком году был изобретён первый iPhone?',
+      answerOptions: [
+        { answerText: '2001', isCorrect: false },
+        { answerText: '2004', isCorrect: false },
+        { answerText: '2007', isCorrect: true },
+        { answerText: '2011', isCorrect: false }
+      ]
+    },
+    {
+      questionText: 'Сколько л/с в Т34?',
+      answerOptions: [
+        { answerText: 'около 200', isCorrect: false },
+        { answerText: 'около 700', isCorrect: false },
+        { answerText: 'около 4000', isCorrect: false },
+        { answerText: 'около 500', isCorrect: true }
+      ]
+    },
+    {
+      questionText: 'В каком году началась первая мировая война?',
+      answerOptions: [
+        { answerText: '1914', isCorrect: true },
+        { answerText: '1900', isCorrect: false },
+        { answerText: '1899', isCorrect: false },
+        { answerText: '1931', isCorrect: false }
+      ]
+    }
+  ]
+
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [score, setScore] = useState(0)
+  const [showScore, setShowScore] = useState(false)
+
+  const handleAnswerOptionClick = (isCorrect: boolean) => {
+
+    if (isCorrect) {
+      setScore(score + 1)
+    }
+
+    const nextQuestion = currentQuestion + 1
+
+    if (nextQuestion < questions.length) {
+
+      setCurrentQuestion(nextQuestion)
+
+    } else {
+
+      setShowScore(true)
+
+    }
+
+  }
+
+  const refresh = () => {
+
+    setCurrentQuestion(0)
+    setScore(0)
+    setShowScore(false)
+
+  }
+
+  if (score < questions.length) {
+    var a = 'Пробуй ещё'
+  } else {
+    var a = 'Красава'
+  }
+
+  return (
+    <div className="app" >
+      {
+        showScore
+          ?
+          <div className="section__score">
+            <div>Правильных ответов {score} из {questions.length}</div>
+            <div className="status">Так как Вы набрали {score} ответов из {questions.length}, Ваш статус - {a}</div>
+            <button className="refresh__btn" onClick={refresh} >Попробовать еще раз</button>
+            </div>
+            :  <div className='quizz'>
+              <div className='question-section'>
+                <div className="question-count">
+                  <span>Вопрос {currentQuestion + 1} </span> /{questions.length}
+                </div>
+                <div className="question-text">{questions[currentQuestion].questionText}
+                </div>
+                <div className="answer-section">
+                  {questions[currentQuestion].answerOptions.map(item =>
+                  (<button
+
+                    onClick={() => handleAnswerOptionClick(item.isCorrect)}
+
+                  >{item.answerText}</button>
+                  )
+                )}
+                </div>
+              </div>
+      
+            </div>
+}</div>
+      
+
+  );
+}
+
+
+export default App;
+
 
