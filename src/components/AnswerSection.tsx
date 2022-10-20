0af9b0cee34a2395a389b4f0d01053b5b9fdc811
Viacheslav <codeexplorer7@gmail.com>
2022-10-20 11:42:18 +0300

    AnswerSection editing 1

--- src/components/AnswerSection.tsx
+++ src/components/AnswerSection.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const AnswerSection = (props: any) => {
-    return (<div className="answer-section">
+  return (<div className="answer-section">
     {props.questions.answerOptions.map((item: { isCorrect: any; answerText: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }) => (<button
       onClick={() => props.handleAnswerOptionClick(item.isCorrect)}
     >{item.answerText}</button>
